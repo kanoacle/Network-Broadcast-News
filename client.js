432c4b941b6f56547ba0308ff952d1bff2a4382d
Kanojah <kanoa.hi@gmail.com>
2017-04-13 11:29:20 -1000

    admin is in and you get notifies in someone leaves

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 /*jshint esversion:6 */
 const net = require('net');
 const client = net.connect({port: 6969}, () => {
+  client.write(`connected`);
   process.stdin.on('data', (chunk) => {
-    client.write(`${chunk.toString()}\n`);
+    client.write(`${chunk.toString()}`);
   });
   client.on('data', (data) => {
     console.log(data.toString());
--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const net = require(`net`);
 const connects = [];
 const server = net.createServer((connection) => {
   connects.push(connection);
+  for (let i = 0; i < connects.length; i++) {
+    if (connects[i] === connection) {
+      connection.write(`You are user ${connects.indexOf(connection) + 1}`);
+    }
+  }
   connection.on(`data`, (data) => {
     console.log(data.toString());
-    for (var i = 0; i < connects.length; i++) {
+    for (let i = 0; i < connects.length; i++) {
       if (connects[i] !== connection) {
         connects[i].write(`\nUser ${connects.indexOf(connection) + 1}: ${data.toString()}`);
       }
   }
   });
-  connection.on(`end`, () => {
-    console.log(`Connection closed, goodbye.`);
+  connection.on('end', () => {
+    for (let y = 0; y < connects.length; y++) {
+      if (connects[y] !== connection) {
+        connects[y].write(`\nUser ${connects.indexOf(connection) + 1} has left the chat.`);
+      }
+    }
+  });
+  process.stdin.on('data', (chunk) => {
+    connection.write(`\nAdmin: ${chunk.toString()}`);
   });
 });
 server.listen(6969, () => {
